f8268aa12be580b3b5fd2fd6d87c8c65af1dd5a0
malabo1990 <malabo1990@gmail.com>
2019-11-13 23:03:34 +0100

    Add files via upload

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author Casimiro Ondo Obiang casimiro.ondo.alum@iescamp.es
+ */
+public class Main {
+    public static void main(String[] args) {
+    
+        //Ejercicio: 02201-TallaZapatoConsola
+        final double CAMBIO_EURO_DOLAR = 1.11;
+        System.out.printf(Locale.ENGLISH, "El cambio de hoy es 1€ por %.2f$ %n", CAMBIO_EURO_DOLAR);
+    }
+}
